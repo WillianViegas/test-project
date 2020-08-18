86775206516d3788a109ce735157b3d492d7ca18
Will <willianwed98@gmail.com>
2020-08-18 17:06:13 -0300

    RetornaIdadeUsuario and others tests

--- src/main/java/services/UsuarioService.java
+++ src/main/java/services/UsuarioService.java
@@ -1,11 +1,44 @@
 package services;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import entities.Usuario;
 
 public class UsuarioService {
 
 
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	public String retornaNomeUsuario(Usuario usuario) {
 		return usuario.getNome();
 	}
+
+	public String retornaDataNascimentoUsuario(Usuario usuario) {
+		return sdf.format(usuario.getDataNasc());
+	}
+
+	public Float retornaAlturaUsuario(Usuario usuario) {
+		return usuario.getAltura();
+	}
+
+	public int retornaIdadeUsuario(Usuario usuario) {
+		Calendar dataNasc = new GregorianCalendar();
+		dataNasc.setTime(usuario.getDataNasc());
+		
+		Calendar dataHoje = Calendar.getInstance();
+		
+		int idade = dataHoje.get(Calendar.YEAR) - dataNasc.get(Calendar.YEAR);
+		
+		dataNasc.add(Calendar.YEAR, idade);
+		
+		if(dataHoje.before(dataNasc)) {
+			idade --;
+		}
+		return idade;
+	}
+	
+	
+	
 }
--- src/test/java/services/UsuarioServiceTest.java
+++ src/test/java/services/UsuarioServiceTest.java
@@ -1,5 +1,7 @@
 package services;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.hamcrest.CoreMatchers;
@@ -32,6 +34,41 @@ public class UsuarioServiceTest {
 		//verificacao
 		error.checkThat(resultado, CoreMatchers.is("Usuario1"));
 		error.checkThat(resultado, CoreMatchers.notNullValue());
+	} 
+	
+	@Test
+	public void deveRetornarDataNascimentoUsuario() {
+		//cenario
+		Usuario usuario1 = new Usuario("Usuario1", new Date(), 1.78f);
+		//acao
+		String resultado = uService.retornaDataNascimentoUsuario(usuario1);
+		//verificacao
+		error.checkThat(resultado, CoreMatchers.is("18/08/2020"));
 	}
-
+	
+	@Test
+	public void deveRetornarAlturaUsuario() {
+		//cenario
+		Usuario usuario1 = new Usuario("Usuario1", new Date(), 1.78f);
+		
+		//acao
+		float resultado = uService.retornaAlturaUsuario(usuario1);
+		
+		//verificacao
+		error.checkThat(resultado, CoreMatchers.is(1.78f));
+	}
+	
+	@Test
+	public void deveRetornarAIdadeDoUsuario() throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		//cenario
+		Usuario usuario1 = new Usuario("Usuario1", sdf.parse("28/07/1998"), 1.78f);
+		
+		//acao
+		int resultado = uService.retornaIdadeUsuario(usuario1);
+		
+		//verificacao
+		error.checkThat(resultado, CoreMatchers.is(22));
+	}
 }
